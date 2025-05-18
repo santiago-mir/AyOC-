4f5566fb2b3dcf62a136ace9dc40062922c06452
santiago-mir <san_chan97@hotmail.com>
2025-05-18 17:37:00 -0300

    add 2025 1c

--- primeros-parciales/2023C2R/solucion/ej2/ej2.asm
+++
@@ -1,7 +0,0 @@
-global combinarImagenes_asm
-
-;########### SECCION DE TEXTO (PROGRAMA)
-section .text
-
-combinarImagenes_asm:
-    ret
--- primeros-parciales/2023C2R/solucion/ej2/ej2.c
+++
@@ -1,5 +0,0 @@
-#include "ej2.h"
-
-void combinarImagenes(uint8_t *src_a, uint8_t *src_b, uint8_t *dst, uint32_t width, uint32_t height)
-{
-}
